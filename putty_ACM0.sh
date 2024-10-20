b8c4cbb578f874b9a3c51753acb1fdcebcb9e2ba
JoeMudr <gh@joesuse.de>
2024-10-20 18:45:46 +0200

    Changed Profile Name

--- putty_ACM0.sh
+++ putty_ACM0.sh
@@ -3,7 +3,7 @@ PORT=/dev/ttyACM0
 while :
 do
 	sleep 1
-	if [ -e "$PORT" ] && [ -z "$(pgrep -f "putty -load ACM0_Teensy")" ] ; then
+	if [ -e "$PORT" ] && [ -z "$(pgrep -f "putty -load ACM0")" ] ; then
 		echo "restart"
 		putty -load ACM0
 	fi
